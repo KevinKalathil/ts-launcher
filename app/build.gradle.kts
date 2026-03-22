aba00100f5ace97be28348c178bceb83ac805587
Kevin Kalathil <kkalathil1@gmail.com>
2026-03-22 02:28:42 -0700

    name change again, was broken

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.3.20" // Use the appropriate version
+    alias(libs.plugins.kotlin.serialization) // Use the alias, remove the version "2.3.20"
 }
 
 android {
